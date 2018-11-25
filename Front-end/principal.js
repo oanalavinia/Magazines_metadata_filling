2f68e77e87c4be5fb83ae10d08a69602faadfffd
oanalavinia <florean.lavinia@gmail.com>
2018-11-25 23:33:12 +0200

    added popup for upload purpose * this is without functionality so far * arranged already done template of index page * added a minimal presentation page

--- Front-end/principal.js
+++ Front-end/principal.js
@@ -1,20 +1,29 @@
 function readURL(input) {
-    if (input.files && input.files[0]) {
-  
-      var reader = new FileReader();
-  
-      reader.onload = function(e) {
-        $('.image-upload-wrap').hide();
-  
-        $('.file-upload-image').attr('src', e.target.result);
-        $('.file-upload-content').show();
-  
-        $('.image-title').html(input.files[0].name);
-      };
-  
-      reader.readAsDataURL(input.files[0]);
-  
-    } else {
-      removeUpload();
-    }
+  if (input.files && input.files[0]) {
+
+    var reader = new FileReader();
+
+    reader.onload = function(e) {
+      $('.image-upload-wrap').hide();
+
+      $('.file-upload-image').attr('src', e.target.result);
+      $('.file-upload-content').show();
+
+      $('.image-title').html(input.files[0].name);
+    };
+
+    reader.readAsDataURL(input.files[0]);
+
+  } else {
+    removeUpload();
   }
+}
+
+// working with jQuery and Bootstrap modal
+window.onload = function(){ 
+  $('#uploadPopup').modal('show');
+
+  $('#uploadPopup').on('hidden.bs.modal', function (e) {
+    $('#presentation')[0].click();
+  })
+}
